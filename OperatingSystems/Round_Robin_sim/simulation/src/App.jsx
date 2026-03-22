d2c505c1ddfac03816d8007abb425bf2e5c4ff8d
Arch_Alejandro <carrilloalejandro0215@gmail.com>
2026-03-21 20:34:01 -0500

    Simulation is working ok, now working on adding extra feautures

--- OperatingSystems/Round_Robin_sim/simulation/src/App.jsx
+++ OperatingSystems/Round_Robin_sim/simulation/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { use, useState } from "react"
+import './index.css'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   const[quantum , setQuantum] = useState(0)
   const[completed, setCompleted] = useState([])
   const[gant, setGant] = useState([])
+  const[porcentajeUsage, setPorcentajeUsage] = useState(0)
 
   function handleName(event){setName(event.target.value)}
 
@@ -56,7 +58,7 @@ function App() {
         arrayGraphic.push({
           name : current.Name,
           start : currentTime,
-          end : currentTime + quantum
+          end : Number(currentTime) + Number(quantum)
         })
 
         current.Time_left = Number(current.Time_left) - Number(quantumEntered)
@@ -67,7 +69,7 @@ function App() {
         arrayGraphic.push({
           name : current.Name,
           start : currentTime,
-          end : currentTime + current.Time_left
+          end : Number(currentTime) + Number(current.Time_left)
         })
         currentTime = Number(currentTime) + Number(current.Time_left)// In case a remaining of 2 when quantum is 3 or more 
         current.Completed = true
@@ -75,40 +77,69 @@ function App() {
         current.Wait_time = Number(current.Time_taken) - Number(current.Time_required) // Tiempo que estuvo idle
         queueCompleted.push(current)
       }
+
     }
+    const cpuUsage = (arrayGraphic.reduce((acumulado,bloque) => {
+      return acumulado + (bloque.end-bloque.start) // Toma cada bloque del gant y suma los tiempos de uso
+    },0)) // Empieza en 0 
+    setPorcentajeUsage((cpuUsage / currentTime )* 100)
+
+
     setGant(arrayGraphic)
     setCompleted(queueCompleted)
     setQuantum(0)
-    console.log(gant)
-  }
 
+  }
+  const totalTime = gant.length > 0 ? gant[gant.length-1].end : 0 // SI hay gant data take it, otherwise 0 
+  const colors = ["bg-amber-500", "bg-blue-500", "bg-green-500", "bg-red-500", "bg-purple-500"]
 
   return (
     <>
-      <h1>Simulacion Procesos - Robin Round </h1>
-      <input value={name} type="text" onChange={handleName} placeholder="Nombre del Proceso" />
-      <input value={ms} type="number" onChange={handleMs} placeholder="Tiempo en ms para realizar el proceso" />
-      <input value={arrivalTime} type="number" onChange={handleArrivalTime} placeholder="Tiempo de llegada (0-)" />
-      <button onClick={handleSubmit} >Submit</button>
+    <div className="flex flex-col text-center bg-gray-200 m-5 gap-3 p-5 items-center">
 
-      <ul>
+      <h1 className="font-extrabold text-6xl text-center" >Simulacion Procesos - Robin Round </h1>
+      <input className="bg-gray-400 rounded-2xl p-3" value={name} type="text" onChange={handleName} placeholder="Nombre del Proceso" />
+      <label htmlFor="">Time to take (ms) </label>
+      <input className="bg-gray-400 rounded-2xl p-3" value={ms} type="number" onChange={handleMs} placeholder="Time in ms" />
+      <label htmlFor="">Arrival time (ms) </label>
+      <input className="bg-gray-400 rounded-2xl p-3" value={arrivalTime} type="number" onChange={handleArrivalTime} placeholder="Time of arrival (0-)" />
+      <button className="bg-green-300 p-4 rounded-2xl shadow-2xl font-bold" onClick={handleSubmit} >Submit </button>
+
+      <ul className="" >
         {proceses.map((proceso,index)=>{
           return(
-            <li key={index}>Name: {proceso.Name} / Ram Usage : {proceso.Ram_used} / Time Required : {proceso.Time_required} / Arrival Time : {proceso.Arrival_time}</li>
+            <li className="bg-white rounded-lg p-3 mb-2" key={index}>Name: {proceso.Name} / Ram Usage : {proceso.Ram_used} / Time Required : {proceso.Time_required} / Arrival Time : {proceso.Arrival_time}</li>
           )
         })}
       </ul>
 
-      <input type="number" value={quantum} onChange={handleQuantum} placeholder="Ingresa el tiempo del Quantum" />
-      <button onClick={handleSimulation} > Simular </button>
+      <input className="bg-gray-400 rounded-2xl p-3" type="number" value={quantum} onChange={handleQuantum} placeholder="Quantum" />
+      <button className="rounded-2xl bg-green-300 p-4 shadow-2xl font-bold" onClick={handleSimulation} > Simulate </button>
       
-      <ul>
+      <ul className="mb-3">
         {completed.map((proceso,index)=>{
           return(
-            <li key={index}>Name: {proceso.Name} / Time Taken : {proceso.Time_taken} / Wait Time (idle) : {proceso.Wait_time} / Time Required: {proceso.Time_required}</li>
+            <li className="bg-white rounded-lg p-3 mb-2" key={index}>Name: {proceso.Name} / Time Taken : {proceso.Time_taken} / Wait Time (idle) : {proceso.Wait_time} / Time Required: {proceso.Time_required}</li>
           )
         })}
       </ul>
+    </div>
+    <div className="m-5 p-5">
+      <label htmlFor="">Usage of the CPU : {porcentajeUsage} % </label>
+      <div className="h-20 text-center border-2 flex w-full">
+        {gant.map((procesoGant, index)=>{
+          const start = procesoGant.start
+          const end = procesoGant.end
+          const porcentaje = ((end - start) / totalTime)*100
+          console.log(porcentaje,"%")
+          const indexColor = proceses.findIndex(objeto => objeto.Name === procesoGant.name) // compáro el name del obj de la lista de procceses, con el nombre del proceso que vamos a dibujar
+          return(
+            <div key={index} style={{ width: `${porcentaje}%`}} className={colors[indexColor]} >{procesoGant.name}: {start}ms-{end}ms</div>
+          )
+        })}
+      </div>
+      
+    </div>
     </>
   )
 }
