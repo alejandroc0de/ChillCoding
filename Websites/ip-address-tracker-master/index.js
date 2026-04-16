a624324c6ce4fd242fac5952a0bb9db1f1066dad
Alejandro <carrilloalejandro0215@gmail.com>
2026-04-16 01:42:49 -0500

    Map is now working adding a pin too

--- Websites/ip-address-tracker-master/index.js
+++ Websites/ip-address-tracker-master/index.js
@@ -1,9 +1,8 @@
 const ip_input = document.getElementById("ip-address")
 const button = document.getElementById("submitButton")
 
-let map = document.getElementById("map")
 
-map = L.map('map').setView([4.7, 74.0], 13);
+const map = L.map('map').setView([4.7, -74.0], 5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -16,10 +15,13 @@ button.addEventListener("click", async (event) => {
     try {
         const res = await fetch (`https://geo.ipify.org/api/v2/country,city?apiKey=at_JzI5TCYLHi7Vs1Ft36eSHBwgEADSO&ipAddress=${ip}`)
         const data = await res.json()
+        console.log(data)
         document.getElementById("ip").textContent = data.ip
         document.getElementById("location").textContent = `${data.location.city}, ${data.location.region}`
         document.getElementById("timezone").textContent = `UTC : ${data.location.timezone}`
         document.getElementById("isp").textContent = data.isp
+        map.setView([data.location.lat, data.location.lng],10)
+        var marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
     } catch (error) {
         console.log(error)
     }
